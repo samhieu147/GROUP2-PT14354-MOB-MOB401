f394054b4cb3d337a40f9002f0fdbca5241e2a49
Hieubdph08068 <Hieubdph08068@fpt.du.vn>
2020-06-12 22:48:09 +0700

    hieubdph08068

--- AppQuanLiChiTieu/app/src/main/java/com/example/develop/appquanlichitieu/TabPage/LoaiThuFragment.java
+++ AppQuanLiChiTieu/app/src/main/java/com/example/develop/appquanlichitieu/TabPage/LoaiThuFragment.java
@@ -28,7 +28,7 @@ public class LoaiThuFragment extends Fragment {
     RecyclerView recyclerView_LoaiThu;
 
     FloatingActionButton btnFab;
-    FButton btnAdd,btnHuy;
+    FButton btnAdd, btnHuy;
     MaterialEditText edtName;
     LoaiThu loaiThu;
     DatabaseLoaiThu databaseLoaiThu;
@@ -36,19 +36,20 @@ public class LoaiThuFragment extends Fragment {
     List<LoaiThu> listdata;
 
     RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view=inflater.inflate(R.layout.fragment_loai_thu,container,false);
-        listdata=new ArrayList<>();
-        recyclerView_LoaiThu =view.findViewById(R.id.recyclerview_loaithu);
+        View view = inflater.inflate(R.layout.fragment_loai_thu, container, false);
+        listdata = new ArrayList<>();
+        recyclerView_LoaiThu = view.findViewById(R.id.recyclerview_loaithu);
         recyclerView_LoaiThu.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(getContext());
+        layoutManager = new LinearLayoutManager(getContext());
         recyclerView_LoaiThu.setLayoutManager(layoutManager);
-        btnFab=view.findViewById(R.id.fab);
+        btnFab = view.findViewById(R.id.fab);
 
-        databaseLoaiThu=new DatabaseLoaiThu(getContext());
+        databaseLoaiThu = new DatabaseLoaiThu(getContext());
 
         LoadDataLoaiThu();
         btnFab.setOnClickListener(new View.OnClickListener() {
@@ -63,15 +64,15 @@ public class LoaiThuFragment extends Fragment {
 
     private void ShowDialogAdd() {
 
-        final Dialog add_loaithu_layout=new Dialog(getContext());
+        final Dialog add_loaithu_layout = new Dialog(getContext());
         add_loaithu_layout.setTitle("Thêm Loại Thu ");
         add_loaithu_layout.setCancelable(false);
 
         add_loaithu_layout.setContentView(R.layout.new_item_loaithu);
         add_loaithu_layout.getWindow().setBackgroundDrawableResource(R.color.colorWhite);
-        btnAdd=add_loaithu_layout.findViewById(R.id.btnAdd);
-        btnHuy=add_loaithu_layout.findViewById(R.id.btnHuy);
-        edtName=add_loaithu_layout.findViewById(R.id.edtName);
+        btnAdd = add_loaithu_layout.findViewById(R.id.btnAdd);
+        btnHuy = add_loaithu_layout.findViewById(R.id.btnHuy);
+        edtName = add_loaithu_layout.findViewById(R.id.edtName);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,21 +91,20 @@ public class LoaiThuFragment extends Fragment {
     }
 
     private void AddNew() {
-//        loaiThu=new LoaiThu(edtName.getText().toString().trim());
-        loaiThu=new LoaiThu(edtName.getText().toString());
-        long chekc=databaseLoaiThu.AddItem(loaiThu);
-        if(chekc>0&&edtName.length()>0){
 
+        if (edtName.getText().toString().isEmpty()) {
+            Toast.makeText(getContext(), "Vui lòng k để trống", Toast.LENGTH_SHORT).show();
+        } else {
+            loaiThu = new LoaiThu(edtName.getText().toString());
+            long check = databaseLoaiThu.AddItem(loaiThu);
             LoadDataLoaiThu();
             Toast.makeText(getContext(), "Thêm Thành Công !", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(getContext(), "Thêm Thất Bại !!", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void LoadDataLoaiThu() {
-        listdata=databaseLoaiThu.getLoaiThu();
-        adapter=new LoaiThuAdapter(listdata,getContext());
+        listdata = databaseLoaiThu.getLoaiThu();
+        adapter = new LoaiThuAdapter(listdata, getContext());
         recyclerView_LoaiThu.setAdapter(adapter);
         adapter.notifyDataSetChanged();
 
